02feccfe8f84a14e28fda7cffcf77288b053d49f
SoproniukAnna <124136300+SoproniukAnna@users.noreply.github.com>
2024-02-12 17:22:35 +0200

    Task 1

--- Lab3CSharp/Program.cs
+++ Lab3CSharp/Program.cs
@@ -1,10 +1,52 @@
-﻿namespace Lab3CSharp
+﻿using System.Diagnostics;
+
+namespace Lab3CSharp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Lab 3 ");
+            Console.WriteLine("         Lab 3 ");
+            Console.WriteLine("         Task 1 ");
+            Trapeze[] trapezes = new Trapeze[]
+                {
+                    new Trapeze(3,3,3,0),
+                    new Trapeze(5,7,2,1),
+                    new Trapeze(8,2,6,2),
+                    new Trapeze(7,9,2,3),
+                    new Trapeze()
+                };
+
+            foreach(var trapez in trapezes) 
+            {
+                trapez.DisplayLengths();
+                Console.WriteLine("Color: " + trapez.C);
+                Console.WriteLine("Perimetr: " + trapez.CalculatePerimeter());
+                Console.WriteLine("Area: " + trapez.CalculateArea());
+                Console.WriteLine("Square: " + trapez.IsSquare());
+                Console.WriteLine();
+            }
+
+            Trapeze trapeze = new Trapeze(1, 1, 1, 1);
+
+            Console.Write("A: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            trapeze.A = a;
+
+            Console.Write("B: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            trapeze.B = b;
+            
+            Console.Write("H: ");
+            int h = Convert.ToInt32(Console.ReadLine());
+            trapeze.H = h;
+
+            trapeze.DisplayLengths();
+            Console.WriteLine("Color: " + trapeze.C);
+            Console.WriteLine("Perimetr: " + trapeze.CalculatePerimeter());
+            Console.WriteLine("Area: " + trapeze.CalculateArea());
+            Console.WriteLine("Square: " + trapeze.IsSquare());
+            Console.WriteLine();
         }
     }
 }
---
+++ Lab3CSharp/Trapeze.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3CSharp
+{
+    internal class Trapeze
+    {
+        //поля
+        private int a;
+        private int b;
+        private int h;
+        private int c;
+
+        //конструктори
+        public Trapeze()
+        {
+            this.a = 3;
+            this.b = 5;
+            this.h = 2;
+            this.c = 0;
+        }
+
+        public Trapeze(int osnova1, int osnova2, int height, int color)
+        {
+            this.a = osnova1;
+            this.b = osnova2;
+            this.h = height;
+            this.c = color;
+        }
+        
+        //get i set
+        //public int A { get => this.a; set => this.a = value;}
+        public int A
+        {
+            get { return this.a; }
+            set { this.a = value; }
+        }
+
+        public int B
+        {
+            get { return this.b; }
+            set { this.b = value; }
+        }
+
+        public int H
+        {
+            get { return this.h; }
+            set { this.h = value; }
+        }
+        
+        public int C
+        {
+            get { return this.c; }
+        }
+        
+
+        //методи
+        public void DisplayLengths()
+        {
+            Console.WriteLine($"Base 1: {A}");
+            Console.WriteLine($"Base 2: {B}");
+            Console.WriteLine($"Height: {H}");
+        }
+
+        public double CalculatePerimeter()
+        {
+            double P = a + b + 2 * Math.Sqrt(Math.Pow((b - a) / 2, 2) + Math.Pow(h, 2));
+            return P;
+        }
+
+        public double CalculateArea()
+        {
+            double S = ((double)this.a + this.b) / 2 * this.h;
+            return S;
+        }
+
+        public bool IsSquare()
+        {
+            return this.a == this.b && this.a == this.h;
+        }
+    }
+}
